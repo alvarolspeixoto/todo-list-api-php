b8493d48ea54a205b0132896bda9c352eecc3398
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2024-06-12 22:04:09 -0300

    feat: create query functions for user model

--- models/User.php
+++ models/User.php
@@ -2,14 +2,48 @@
 
 namespace Alvaro\TodoPhp\models;
 
+use Alvaro\TodoPhp\config\Connection;
+
 class User extends Model {
 
+    protected $db;
     protected $table;
     protected $primaryKey;
+    protected $fillable;
 
     public function __construct() {
+        $this->db = Connection::getConnection();
         $this->table = 'user';
         $this->primaryKey = 'id';
+        $this->fillable = [
+            'username',
+            'email',
+            'password'
+        ];
     }
 
+    public function findByUsername(string $username)
+    {
+        $table = $this->table;
+        $stmt = $this->db->prepare("SELECT * FROM $table
+                                WHERE username = :username");
+        $stmt->execute(['username' => $username]);
+        $result = $stmt->fetch();
+
+        return $result;
+    }
+
+    // criar uma função geral na model base que faça o papel de buscar por qualquer chave única
+    // findByKey($key, $value)
+    public function findByEmail(string $email)
+    {
+
+        $table = $this->table;
+        $stmt = $this->db->prepare("SELECT * FROM $table
+                                WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        $result = $stmt->fetch();
+
+        return $result;
+    }
 }
