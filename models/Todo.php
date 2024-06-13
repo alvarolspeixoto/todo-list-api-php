0ea2a387ee10a56b1045669b1486c4dfc79d383c
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2024-06-12 22:26:10 -0300

    feat: implement todo model

--- models/Todo.php
+++ models/Todo.php
@@ -2,6 +2,25 @@
 
 namespace Alvaro\TodoPhp\models;
 
-class Todo {
+use Alvaro\TodoPhp\config\Connection;
+
+class Todo extends Model {
     
+    protected $db;
+    protected $table;
+    protected $primaryKey;
+    protected $fillable;
+
+    public function __construct() {
+        $this->db = Connection::getConnection();
+        $this->table = 'todo';
+        $this->primaryKey = 'id';
+        $this->fillable = [
+            'title',
+            'description',
+            'status',
+            'user_id',
+        ];
+    }
+
 }
