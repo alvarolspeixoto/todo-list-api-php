0213152ddc4293e39b2b419bd012ef6a24e0145c
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2024-06-12 22:05:38 -0300

    feat: implement create function for base model

--- models/Model.php
+++ models/Model.php
@@ -2,44 +2,71 @@
 
 namespace Alvaro\TodoPhp\models;
 
-use Alvaro\TodoPhp\config\Connection;
+use Exception;
 
-abstract class Model {
+abstract class Model
+{
 
+    protected $db;
     protected $table;
     protected $primaryKey;
+    protected $fillable;
 
-    public function findAll() {
-
-        $conn = Connection::getConnection();
-
+    public function findAll()
+    {
         $table = $this->table;
-        $stmt = $conn->prepare("SELECT * FROM $table");
+        $stmt = $this->db->prepare("SELECT * FROM $table");
         $stmt->execute();
 
-        return $stmt->fetchAll(); 
+        return $stmt->fetchAll();
     }
 
-    public function findById(int $id) {
-
-        $conn = Connection::getConnection();
-
+    public function findById(int $id)
+    {
         $table = $this->table;
         $pk = $this->primaryKey;
-        $stmt = $conn->prepare("SELECT * FROM $table
+        $stmt = $this->db->prepare("SELECT * FROM $table
                                 WHERE $pk = :id");
         $stmt->execute(['id' => $id]);
         $result = $stmt->fetch();
 
-        return $result ? $result : [];
+        return $result;
     }
 
-    public function create(array $data) {
+    public function create(array $data)
+    {
+        $attributes = array_filter(
+            array_keys($data),
+            fn ($attr) => in_array($attr, $this->fillable)
+        );
 
+        if (count($this->fillable) > count($attributes)) {
+            $missingAttributes = array_diff($this->fillable, $attributes);
+            $errorMessage = "Os seguintes atributos são requeridos: " . implode(", ", $missingAttributes);
+            throw new Exception($errorMessage, 400);
+        }
+
+        $table = $this->table;
+        $fields = implode(', ', $attributes);
+
+        $bindings = [];
+
+        foreach ($attributes as $attr) {
+            $bindings[] = ":$attr";
+        }
+
+        $bindings = implode(', ', $bindings);
+        $stmt = $this->db->prepare("INSERT INTO $table ($fields)
+                                    VALUES ($bindings);");
+
+        foreach ($data as $key => $value) {
+            if (in_array($key, $this->fillable)) $stmt->bindValue(":$key", $value);
+        }
+
+        return $stmt->execute();
     }
 
-    public function update(int $id, array $data) {
-
+    public function update(int $id, array $data)
+    {
     }
-
 }
